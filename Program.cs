482db2dbded73ec21705fce027ca427856edd466
PedroIRS <pedroitalo123765@gmail.com>
2025-08-25 19:06:39 -0300

    Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,8 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Iphone cll1 = new Iphone;
+Nokia cll2 = new Nokia;
+
+cll1.InstalarAplicativo("App");
+cll1.InstalarAplicativo("App");
