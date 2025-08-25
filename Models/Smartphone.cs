2d38ef881f6b7ee3ba72dd6c8c0705e1953a704f
PedroIRS <pedroitalo123765@gmail.com>
2025-08-25 18:59:28 -0300

    Smartphone.cs

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -4,10 +4,16 @@ namespace DesafioPOO.Models
     {
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        public string Modelo { get; set; }
+        public string IMEI { get; set; }
+        public int Numero { get; set; }
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int numero)
         {
             Numero = numero;
+            Modelo = modelo;
+            IMEI = imei;
+            Numero = Numero;
             // TODO: Passar os parâmetros do construtor para as propriedades
         }
 
